7db002c7605022b7095c918e3f8f3e98fff3c31b
Dan Alloway <dan@micahsix.com>
2021-11-19 13:32:18 -0500

    fix test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { ReactCountryFlag } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<ReactCountryFlag countryCode="US" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
